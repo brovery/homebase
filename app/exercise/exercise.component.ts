e83e112667beb297f504beb446a731b7f0e2a60a
Brandon O'Very <brovery@yahoo.com>
2016-09-21 17:16:30 -0600

    Progress on exercise setup.

--- app/exercise/exercise.component.ts
+++ app/exercise/exercise.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { EXERCISE, EXERCISEDETAILS } from './mock-exercises';
 
 @Component({
     selector: 'exercise',
@@ -8,7 +9,17 @@ import { Router } from '@angular/router';
 export class ExerciseComponent implements OnInit {
     constructor(private router: Router) {}
 
+    // Declare variables here.
+    program = 'P90X';
+    exercises = EXERCISE;
+    details = EXERCISEDETAILS[0];
+
+    // Component Methods go here.
     ngOnInit(): void {
 
+    }
+
+    logit(): void {
+        console.log(this.details);
     }
 }
